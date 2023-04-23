477ff265d91d347b99f329ad1771dedda92e8b9d
AlbertoJTD <albertojtd@outlook.com>
2023-04-23 16:20:00 -0600

    Add unescape to decode HTML entities

--- quiz_brain.py
+++ quiz_brain.py
@@ -1,18 +1,22 @@
+import html
+
 class QuizBrain:
     def __init__(self, q_list):
         self.question_number = 0
         self.question_list = q_list
         self.score = 0
+        self.current_question = None
 
     def still_has_questions(self):
         return self.question_number < len(self.question_list)
 
     def next_question(self):
-        current_question = self.question_list[self.question_number]
+        self.current_question = self.question_list[self.question_number]
         self.question_number += 1
-        choice = input(f"Q.{self.question_number}: {current_question.text} (True/False) > ")
+        question = html.unescape(self.current_question.text)
+        choice = input(f"Q.{self.question_number}: {question} (True/False) > ")
 
-        self.check_answer(choice, current_question.answer)
+        self.check_answer(choice, self.current_question.answer)
 
     def check_answer(self, choice, answer):
         if choice.lower() == answer.lower():
