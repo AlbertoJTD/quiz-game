ccd40bd68a361e37510909914ca509fafce8f264
AlbertoJTD <albertojtd@outlook.com>
2023-04-24 14:01:48 -0600

    Add GUI

--- ui.py
+++ ui.py
@@ -6,4 +6,22 @@ class QuizInterface:
     def __init__(self):
         self.window = Tk()
         self.window.title('Quiz Game')
+        self.window.config(padx=20, pady=20, bg=THEME_COLOR)
+
+        self.score_label = Label(text='Score: 0', bg=THEME_COLOR, fg='white')
+        self.score_label.grid(row=0, column=1)
+
+        self.canvas = Canvas(width=300, height=250, bg='white')
+        self.question = self.canvas.create_text(150, 125, text='Some text here', width=250, font=("Arial", 20, "italic"), fill=THEME_COLOR)
+        self.canvas.grid(row=1, column=0, columnspan=2, pady=50)
+
+        true_img = PhotoImage(file="images/true.png")
+        self.true_button = Button(image=true_img, highlightthickness=0, relief="flat")
+        self.true_button.grid(row=2, column=0)
+
+        false_img = PhotoImage(file="images/false.png")
+        self.false_button = Button(image=false_img, highlightthickness=0, relief="flat")
+        self.false_button.grid(row=2, column=1)
+
+
         self.window.mainloop()
