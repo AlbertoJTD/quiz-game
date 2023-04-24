ed39350fd1500a3046f4200b485be4fbc394e4bd
AlbertoJTD <albertojtd@outlook.com>
2023-04-24 13:15:08 -0600

    Add QuizInterface class

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from question_model import Question
 from data import question_data
 from quiz_brain import QuizBrain
+from ui import QuizInterface
 
 # create an empty list
 question_bank = []
@@ -11,9 +12,10 @@ for question in question_data:
 
 # Create a QuizBrain object and give it as an argument the list
 quiz = QuizBrain(question_bank)
+quiz_ui = QuizInterface()
 
-while quiz.still_has_questions():
-    quiz.next_question()
+# while quiz.still_has_questions():
+#     quiz.next_question()
 
 # Print your final score
 quiz.result()
